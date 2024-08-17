6a8ecdef8f79930d478e175f8f4e60d82b0e2733
Achref Awaissi <achrefawaissi@gmail.com>
2024-08-17 10:59:48 -0700

    Création de module filter

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './databse.module';
 import { ItemModule } from './item/item.module';
+import { FilterModule } from './filter/filter.module';
 
 @Module({
-  imports: [DatabaseModule, ItemModule],
+  imports: [DatabaseModule, ItemModule, FilterModule],
   controllers: [],
   providers: [],
 })
