6406f827d64eaffb28efb6c12ae2c6dc422614f1
Achref Awaissi <achrefawaissi@gmail.com>
2024-08-16 23:50:22 -0700

    Ajout de la logique metier des opération crud sur la db

--- Backend/src/item/item.controller.ts
+++ Backend/src/item/item.controller.ts
@@ -1,4 +1,99 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UploadedFile,
+  UseInterceptors,
+  NotFoundException,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ItemService } from './item.service';
+import { Item } from './item.iterface';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('item')
-export class ItemController {}
+export class ItemController {
+  constructor(private readonly itemService: ItemService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+        },
+      }),
+    }),
+  )
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() item: Item,
+  ): Promise<Item> {
+    if (file) {
+      item.image = file.filename;
+    }
+    console.log('Received item:', item);
+    return this.itemService.create(item);
+  }
+
+  @Get()
+  async findAll(): Promise<Item[]> {
+    return this.itemService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Item> {
+    const item = await this.itemService.findOne(id);
+    if (!item) {
+      throw new NotFoundException(`Item with ID ${id} not found`);
+    }
+    return item;
+  }
+
+  @Put(':id')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+        },
+      }),
+    }),
+  )
+  async update(
+    @Param('id') id: string,
+    @UploadedFile() file: Express.Multer.File,
+    @Body() item: Item,
+  ): Promise<Item> {
+    if (file) {
+      item.image = file.filename; // Met à jour l'image si un fichier est fourni
+    }
+    const updatedItem = await this.itemService.update(id, item);
+    if (!updatedItem) {
+      throw new NotFoundException(`Item with ID ${id} not found`);
+    }
+    return updatedItem;
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Item> {
+    const deletedItem = await this.itemService.deleteById(id);
+    if (!deletedItem) {
+      throw new NotFoundException(`Item with ID ${id} not found`);
+    }
+    return deletedItem;
+  }
+}
